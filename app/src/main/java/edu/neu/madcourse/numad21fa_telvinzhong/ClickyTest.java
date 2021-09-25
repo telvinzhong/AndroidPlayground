99a67fd4308d3dac18b5b52651e6db692e1fdc12
Telvin Zhong <telvinzhong@gmail.com>
2021-09-25 10:56:43 -0700

    letter shows properly upon tap and hold

--- app/src/main/java/edu/neu/madcourse/numad21fa_telvinzhong/ClickyTest.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_telvinzhong/ClickyTest.java
@@ -9,14 +9,16 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class ClickyTest extends AppCompatActivity {
-    TextView lastClick = (TextView)findViewById(R.id.lastClick);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky_test);
 
-        lastClick.setText("-");
+        // lastClick needs to be in onCreate
+        TextView lastClick = (TextView)findViewById(R.id.lastClick);
+        lastClick.setText("Pressed: -");
 
         Button A = (Button)findViewById(R.id.button2);
         A.setOnTouchListener(new View.OnTouchListener() {
@@ -24,10 +26,85 @@ public class ClickyTest extends AppCompatActivity {
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 // whenever the button is pressed down
                 if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    lastClick.setText("A");
+                    lastClick.setText("Pressed: A");
                     // Change back to -
                 } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    lastClick.setText("-");
+                    lastClick.setText("Pressed: -");
+                }
+                return true;
+            }
+        });
+
+        Button C = (Button)findViewById(R.id.button5);
+        C.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // whenever the button is pressed down
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastClick.setText("Pressed: C");
+                    // Change back to -
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    lastClick.setText("Pressed: -");
+                }
+                return true;
+            }
+        });
+
+        Button B = (Button)findViewById(R.id.button4);
+        B.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // whenever the button is pressed down
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastClick.setText("Pressed: B");
+                    // Change back to -
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    lastClick.setText("Pressed: -");
+                }
+                return true;
+            }
+        });
+
+        Button D = (Button)findViewById(R.id.button6);
+        D.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // whenever the button is pressed down
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastClick.setText("Pressed: D");
+                    // Change back to -
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    lastClick.setText("Pressed: -");
+                }
+                return true;
+            }
+        });
+
+        Button E = (Button)findViewById(R.id.button7);
+        E.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // whenever the button is pressed down
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastClick.setText("Pressed: E");
+                    // Change back to -
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    lastClick.setText("Pressed: -");
+                }
+                return true;
+            }
+        });
+
+        Button F = (Button)findViewById(R.id.button8);
+        F.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // whenever the button is pressed down
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastClick.setText("Pressed: F");
+                    // Change back to -
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    lastClick.setText("Pressed: -");
                 }
                 return true;
             }
