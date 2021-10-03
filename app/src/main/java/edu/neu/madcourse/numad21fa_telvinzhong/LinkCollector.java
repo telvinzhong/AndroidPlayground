44306d86a38f4ae9124480591724579cb75ec16b
Telvin Zhong <telvinzhong@gmail.com>
2021-10-03 14:32:04 -0500

    fab now adds items to recyclerView

--- app/src/main/java/edu/neu/madcourse/numad21fa_telvinzhong/LinkCollector.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_telvinzhong/LinkCollector.java
@@ -5,13 +5,20 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class LinkCollector extends AppCompatActivity {
     RecyclerView linkCollector;
     ArrayList names;
     ArrayList urls;
+    private FloatingActionButton addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,21 @@ public class LinkCollector extends AppCompatActivity {
         MyAdapter myAdapter = new MyAdapter(this, names, urls);
         linkCollector.setAdapter(myAdapter);
         linkCollector.setLayoutManager(new LinearLayoutManager(this));
+
+        addButton = findViewById(R.id.floatingActionButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pos = 0;
+                names.add(pos, "test1");
+                urls.add(pos, "www.google.com");
+                Snackbar mySnackbar = Snackbar.make(findViewById(R.id.linkCollector),
+                        "New item added.", Snackbar.LENGTH_SHORT);
+                mySnackbar.show();
+                myAdapter.notifyItemInserted(pos);
+            }
+        });
+
 
     }
 
